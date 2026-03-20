98011a51b665b4f22ced7f53100289996055196a
th1nhng0 <ngophuthinh2001@gmail.com>
2026-03-21 01:56:28 +0700

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "192.168.36.75",
+    "ixbgy-171-244-236-159.a.free.pinggy.link",
+  ],
 };
 
 export default nextConfig;
